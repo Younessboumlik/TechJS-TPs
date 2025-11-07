4453d1190c47598bf6711dfc36bd48c3d8f4d9d7
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2025-11-07 18:40:36 +0100

    Add basic Express app with user auth and book list

---
+++ TpExpress/app.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const {register, login} = require('./passport')
+
+
+let authentification = false;
+let books=[{
+    title:'book1',
+    author:'me',
+    genre:'comedy'
+}]
+
+const app = express()
+
+ app.set("view engine", "pug");
+ app.set("views", "./pages")
+ app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async(req, res) => {
+    username = req.body.username
+    password = req.body.password
+    
+    console.log(username)
+    await register(username, password)
+    res.redirect('register')
+
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    username = req.body.username
+    password = req.body.password
+
+    authentification = await login(username, password)
+
+    if(authentification === true){
+        res.redirect('book')
+    }
+    else{
+        res.redirect('login')
+    }
+})
+
+app.get('/book', authentificationAccess, (req, res) => {
+    res.render('book', {books:books})
+})
+
+app.get('/logout', authentificationAccess, (req, res) => {
+    authentification = false
+
+    res.redirect('login')
+})
+
+
+function authentificationAccess(req, res, next){
+    if(authentification === true){
+        next()
+    }
+    else{
+        res.redirect('login')
+    }
+}
+
+app.listen(5000)
---
+++ TpExpress/passport.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/myappdb')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('DB Connection Error:', err));
+
+
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String});
+
+const User = mongoose.model('User', userSchema);
+
+
+async function register(username, password){
+    const newUser = new User({ username, password });
+    await newUser.save();
+    console.log('user bien enregistré')
+}
+
+
+async function login(username, password){
+
+    const user = await User.findOne({ username: username, password: password });
+
+    if(user === null) return false
+    return true
+    
+}
+module.exports = {register, login}
